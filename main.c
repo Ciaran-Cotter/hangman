00a92d531c812e24bce5de84a38cc0c125a4d7fb
Ciaran Cotter <45354437+Ciaran-Cotter@users.noreply.github.com>
2018-12-06 17:28:27 +0000

    Working Main
    Added choose random word from file functionality.

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 void guess_check(char guess[], char avl_letters[], char guessed_letters[]){/* Function to check if a guess has been made before and if not, add the guess to a list of guess letters. 
 Inputs: Guess, available letters and guessed_letters array.
@@ -21,12 +24,50 @@ Output: nothing
 
 
 
+
 int main(void) { // Start main loop
 
-char word[] = "Hangman";	// Assign main word as Hangman for now
-//printf("%s", word); //DB
+
+FILE *file_pntr; // Creates pointer to point to file
+int x; // Will be used to interate between words
+
+file_pntr = fopen("words.txt", "r"); // Initialises file to pointer
+
+if (!file_pntr){ // Checks if it opened
+			printf("Failed to open");
+			
+}	
+
+char word[475869]; //Creates a buffer where a read string from the txt file can be loaded and printed from. 
+
+char skip[] = " %*s";
+char end[] = " %s";
+char add[] = " %*s";
+
+srand ( time(NULL) ); //Seed the rand() function with time staring at the beggining of time which provides a new seed every game. 
+
+int n = rand() % 55900; // Chooses a random number of words to skip when reading file to buff based on maximum value of 
+
+
+
+int z = 0; // Loop to concantonate string "Skip" with "add" to build directions to skip the number of words selected. 
+for (z=0; z<=n; ++z){
+	strcat(skip, add);
+	}
+
+strcat(skip, end); // Adds the final %s at the end of the string to chose a word after all the skipped words. Its not pretty but it works. 
+
+
+
+fscanf(file_pntr, skip ,word); // reads the file and imports the word to "buff". 
+
+
+
+fclose(file_pntr); // Closes the file. 
+
+
+
 int len = strlen(word);
-//printf("\n%d", len); //DB
 
 //Initialise values
 int attempts = 6; 
@@ -68,9 +109,7 @@ while (attempts > 0){
 	// Now we need to check if it is a correct guess or not. 	
 	if (strchr(word, *guess) == NULL){ //checks if guess has already been used.
 		//If guess is not in word then remove attempt and print incorrect dialogue and restart loop
-		char *test = strchr(word, *guess);
-		printf("%s", test);
-		attempts = attempts - 1;
+			attempts = attempts - 1;
 		printf("\nOops, That letter does not feature in the word I am thinking of!\n\n**********************************\n\nYou have %d attempts left.\n", attempts);
 		//Check if game is over and escape if all attempts are used up. 
 		if (attempts <= 0){
@@ -101,3 +140,5 @@ while (attempts > 0){
 }	
 }		
 			
+
+
